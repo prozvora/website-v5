c392189d7c6e792ec7510760da33d2670627e3da
proz <prozvora@gmail.com>
2024-03-04 18:03:19 -0500

    Redo rubiks data. shrink cube diagram. Fix layout on mobile. simplify display code.

--- src/pages/rubiks.js
+++ src/pages/rubiks.js
@@ -2,23 +2,7 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import RubiksSection from '../components/rubiks_section'
-import {
-  dot,
-  line,
-  cross,
-  fourCorners,
-  square,
-  bolt,
-  fish,
-  wishbone,
-  smallL,
-  bigL,
-  c,
-  p,
-  t,
-  w,
-  z,
-} from '../../static/rubiks_oll_data'
+import { ollPatterns } from '../../static/rubiks_oll_data'
 
 import '../css/rubiks.css'
 
@@ -29,36 +13,18 @@ const Rubiks = () => (
       Click a tile, or tab to a tile and press any key, to toggle showing its
       algorithm.
     </p>
-    <h3>Dot</h3>
-    <RubiksSection data={dot} />
-    <h3>Line</h3>
-    <RubiksSection data={line} />
-    <h3>Cross</h3>
-    <RubiksSection data={cross} />
-    <h3>4 corners</h3>
-    <RubiksSection data={fourCorners} />
-    <h3>Square</h3>
-    <RubiksSection data={square} />
-    <h3>Small Bolt</h3>
-    <RubiksSection data={bolt} />
-    <h3>Fish</h3>
-    <RubiksSection data={fish} />
-    <h3>Wishbone</h3>
-    <RubiksSection data={wishbone} />
-    <h3>Small L</h3>
-    <RubiksSection data={smallL} />
-    <h3>C</h3>
-    <RubiksSection data={c} />
-    <h3>L</h3>
-    <RubiksSection data={bigL} />
-    <h3>P</h3>
-    <RubiksSection data={p} />
-    <h3>T</h3>
-    <RubiksSection data={t} />
-    <h3>W</h3>
-    <RubiksSection data={w} />
-    <h3>Z</h3>
-    <RubiksSection data={z} />
+    <h3>Jump To:</h3>
+    <div className='jumpTo'>
+      {ollPatterns.map((pattern) => (
+        <div className='jumpToHeading'>{pattern.heading}</div>
+      ))}
+    </div>
+    {ollPatterns.map((pattern) => (
+      <>
+        <h3>{pattern.heading}</h3>
+        <RubiksSection data={pattern.patterns} />
+      </>
+    ))}
   </Layout>
 )
 
